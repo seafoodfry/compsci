0827301b22444e3b8dfaf139615f26fc77b49acc
seafoodfry <99568361+seafoodfry@users.noreply.github.com>
2024-08-23 22:01:41 -0400

    Added mechanisms to keep track of indices of max subarray
    Also formatted source code.
    
    Signed-off-by: seafoodfry <99568361+seafoodfry@users.noreply.github.com>

--- algorithms/max-subarray/max-subarray/src/max_subarray.rs
+++ algorithms/max-subarray/max-subarray/src/max_subarray.rs
@@ -1,8 +1,15 @@
-use std::ops::{Add, AddAssign};
 use std::cmp::PartialOrd;
+use std::ops::{Add, AddAssign};
 
+// Derive Debug to print them and PartialEq to test them.
+#[derive(Debug, PartialEq)]
+pub struct SubArrayResult<N> {
+    pub sum: N,
+    pub start: usize,
+    pub end: usize,
+}
 
-pub fn brute_force<N>(nums: &[N]) -> Option<N> 
+pub fn brute_force<N>(nums: &[N]) -> Option<SubArrayResult<N>>
 where
     N: Default + Copy + PartialOrd + Add<Output = N> + AddAssign,
     // Default -> N:default() to provide a zero value.
@@ -11,25 +18,33 @@ where
     // Add<Output = N> -> needed to add values together.
     // AddAssign -> needed for += nums[j].
 {
-    let n = nums.len();
-    if n == 0 {
+    if nums.is_empty() {
         return None;
     }
 
-    let mut max_sum = nums[0];
-    for i in 0..n {
+    let mut max_result = SubArrayResult {
+        // Using N::default() will result in the wrong result if all entries are negative.
+        sum: nums[0],
+        start: 0,
+        end: 0,
+    };
+    for start in 0..nums.len() {
         let mut current_sum = N::default();
-        for j in i..n {
-            current_sum += nums[j];
-            if current_sum > max_sum {
-                max_sum = current_sum;
+
+        for end in start..nums.len() {
+            current_sum += nums[end];
+            if current_sum > max_result.sum {
+                max_result = SubArrayResult {
+                    sum: current_sum,
+                    start,
+                    end,
+                };
             }
         }
     }
 
-    Some(max_sum)
+    Some(max_result)
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -38,7 +53,9 @@ mod tests {
     #[test]
     fn test_brute_force() {
         let vec1 = vec![0, 1, 2, 3, 4, 5];
-        let book = vec![13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7];
+        let book = vec![
+            13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7,
+        ];
 
         // We only need to specify the generic type for this next function because that is the only
         // instance in which the compiler won't be able to infer the type.
@@ -46,12 +63,54 @@ mod tests {
         // We could also do...
         assert_eq!(brute_force(&[] as &[u32]), None);
 
-        assert_eq!(brute_force(&[0]), Some(0));
-        assert_eq!(brute_force(&[0, 0, 0]), Some(0));
+        assert_eq!(
+            brute_force(&[0]),
+            Some(SubArrayResult {
+                sum: 0,
+                start: 0,
+                end: 0
+            })
+        );
+        assert_eq!(
+            brute_force(&[0, 0, 0]),
+            Some(SubArrayResult {
+                sum: 0,
+                start: 0,
+                end: 0
+            })
+        );
 
-        assert_eq!(brute_force(&[1, 2, 3]), Some(6));
-        assert_eq!(brute_force(&vec1), Some(15));
-        assert_eq!(brute_force(&[-1, -2, -3]), Some(-1));
-        assert_eq!(brute_force(&book), Some(43));
+        assert_eq!(
+            brute_force(&[1, 2, 3]),
+            Some(SubArrayResult {
+                sum: 6,
+                start: 0,
+                end: 2
+            })
+        );
+        assert_eq!(
+            brute_force(&vec1),
+            Some(SubArrayResult {
+                sum: 15,
+                start: 0,
+                end: 5
+            })
+        );
+        assert_eq!(
+            brute_force(&[-1, -2, -3]),
+            Some(SubArrayResult {
+                sum: -1,
+                start: 0,
+                end: 0
+            })
+        );
+        assert_eq!(
+            brute_force(&book),
+            Some(SubArrayResult {
+                sum: 43,
+                start: 7,
+                end: 10
+            })
+        );
     }
 }
