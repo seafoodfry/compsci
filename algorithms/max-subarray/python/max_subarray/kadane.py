59a125834978d7286ae2847020bc642b0c51f506
seafoodfry <99568361+seafoodfry@users.noreply.github.com>
2024-08-24 22:22:47 -0400

    Updated tests for the subarray problem
    Signed-off-by: seafoodfry <99568361+seafoodfry@users.noreply.github.com>

--- algorithms/max-subarray/python/max_subarray/kadane.py
+++ algorithms/max-subarray/python/max_subarray/kadane.py
@@ -1,7 +1,44 @@
-def kadane(numbers: list[float | int]) -> float:
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class SubArrayResult:
+    sum: float
+    start: int
+    end: int
+
+
+def kadane(numbers: list[float | int]) -> Optional[float]:
     """Find the largest sum of any contiguous subarray."""
+    if len(numbers) == 0:
+        return None
+
     current_sum = best_sum = numbers[0]
     for x in numbers:
         current_sum = max(x, current_sum + x)
         best_sum = max(best_sum, current_sum)
     return best_sum
+
+
+def kadane_with_indices(numbers: list[float | int]) -> Optional[SubArrayResult]:
+    """Find the largest sum of any contiguous subarray."""
+    if len(numbers) == 0:
+        return None
+
+    current_sum = best_sum = numbers[0]
+    start = end = best_start = 0
+
+    for i, x in enumerate(numbers):
+        if x > current_sum + x:
+            current_sum = x
+            start = i
+        else:
+            current_sum += x
+
+        if current_sum > best_sum:
+            best_sum = current_sum
+            best_start = start
+            end = i
+
+    return SubArrayResult(sum=best_sum, start=best_start, end=end)
--- algorithms/max-subarray/rust/src/max_subarray.rs
+++ algorithms/max-subarray/rust/src/max_subarray.rs
@@ -9,7 +9,7 @@ pub struct SubArrayResult<N> {
     pub end: usize,
 }
 
-pub fn brute_force<N>(nums: &[N]) -> Option<SubArrayResult<N>>
+pub fn brute_force<N>(numbers: &[N]) -> Option<SubArrayResult<N>>
 where
     N: Default + Copy + PartialOrd + Add<Output = N> + AddAssign,
     // Default -> N:default() to provide a zero value.
@@ -18,21 +18,21 @@ where
     // Add<Output = N> -> needed to add values together.
     // AddAssign -> needed for += nums[j].
 {
-    if nums.is_empty() {
+    if numbers.is_empty() {
         return None;
     }
 
     let mut max_result = SubArrayResult {
         // Using N::default() will result in the wrong result if all entries are negative.
-        sum: nums[0],
+        sum: numbers[0],
         start: 0,
         end: 0,
     };
-    for start in 0..nums.len() {
+    for start in 0..numbers.len() {
         let mut current_sum = N::default();
 
-        for (end, _) in nums.iter().enumerate().skip(start) {
-            current_sum += nums[end];
+        for (end, _) in numbers.iter().enumerate().skip(start) {
+            current_sum += numbers[end];
             if current_sum > max_result.sum {
                 max_result = SubArrayResult {
                     sum: current_sum,
@@ -46,25 +46,59 @@ where
     Some(max_result)
 }
 
+pub fn kadane<N>(numbers: &[N]) -> Option<SubArrayResult<N>>
+where
+    N: Default + Copy + PartialOrd + Add<Output = N> + AddAssign,
+    // Default -> N:default() to provide a zero value.
+    // Copy -> needed for += nums[j].
+    // PartialOrd -> needed for comparisons.
+    // Add<Output = N> -> needed to add values together.
+    // AddAssign -> needed for += nums[j].
+{
+    if numbers.is_empty() {
+        return None;
+    }
+
+    let mut max_result = SubArrayResult {
+        sum: numbers[0],
+        start: usize::default(),
+        end: usize::default(),
+    };
+    let mut current_sum = N::default();
+    let mut current_start = usize::default();
+
+    for (idx, &num) in numbers.iter().enumerate() {
+        if current_sum + num >= num {
+            current_sum += num;
+        } else {
+            current_sum = num;
+            current_start = idx;
+        }
+
+        if current_sum > max_result.sum {
+            max_result = SubArrayResult {
+                sum: current_sum,
+                start: current_start,
+                end: idx,
+            };
+        }
+    }
+
+    Some(max_result)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_brute_force() {
-        let vec1 = vec![0, 1, 2, 3, 4, 5];
-        let book = vec![
-            13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7,
-        ];
-
-        // We only need to specify the generic type for this next function because that is the only
-        // instance in which the compiler won't be able to infer the type.
-        assert_eq!(brute_force::<i32>(&[]), None);
-        // We could also do...
-        assert_eq!(brute_force(&[] as &[u32]), None);
+    fn test_cases_i32<F>(algorithm: F)
+    where
+        F: Fn(&[i32]) -> Option<SubArrayResult<i32>>,
+    {
+        assert_eq!(algorithm(&[]), None);
 
         assert_eq!(
-            brute_force(&[0]),
+            algorithm(&[0]),
             Some(SubArrayResult {
                 sum: 0,
                 start: 0,
@@ -72,7 +106,7 @@ mod tests {
             })
         );
         assert_eq!(
-            brute_force(&[0, 0, 0]),
+            algorithm(&[0, 0, 0]),
             Some(SubArrayResult {
                 sum: 0,
                 start: 0,
@@ -81,36 +115,82 @@ mod tests {
         );
 
         assert_eq!(
-            brute_force(&[1, 2, 3]),
+            algorithm(&[1, 2, 3]),
             Some(SubArrayResult {
                 sum: 6,
                 start: 0,
                 end: 2
             })
         );
+
+        let vec1 = vec![0, 1, 2, 3, 4, 5];
+        let book = vec![
+            13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7,
+        ];
+
         assert_eq!(
-            brute_force(&vec1),
+            algorithm(&vec1),
             Some(SubArrayResult {
                 sum: 15,
                 start: 0,
                 end: 5
             })
         );
+
         assert_eq!(
-            brute_force(&[-1, -2, -3]),
-            Some(SubArrayResult {
-                sum: -1,
-                start: 0,
-                end: 0
-            })
-        );
-        assert_eq!(
-            brute_force(&book),
+            algorithm(&book),
             Some(SubArrayResult {
                 sum: 43,
                 start: 7,
                 end: 10
             })
         );
+    }
+
+    fn test_cases_u32(algorithm: fn(&[u32]) -> Option<SubArrayResult<u32>>) {
+        assert_eq!(algorithm(&[] as &[u32]), None);
+    }
+
+    fn test_cases_f64(algorithm: fn(&[f64]) -> Option<SubArrayResult<f64>>) {
+        assert_eq!(
+            algorithm(&[1.1, 2.2, 3.3]),
+            Some(SubArrayResult {
+                sum: 6.6,
+                start: 0,
+                end: 2
+            })
+        );
+
+        assert_eq!(
+            algorithm(&[-1.1, 2.2, 3.3]),
+            Some(SubArrayResult {
+                sum: 5.5,
+                start: 1,
+                end: 2
+            })
+        );
+    }
+
+    #[test]
+    fn explicit_test_cases() {
+        // We only need to specify the generic type for this next function because that is the only
+        // instance in which the compiler won't be able to infer the type.
+        assert_eq!(brute_force::<i32>(&[]), None);
+        // We could also do...
+        assert_eq!(brute_force(&[] as &[u32]), None);
+    }
+
+    #[test]
+    fn test_brute_force() {
+        test_cases_i32(brute_force::<i32>);
+        test_cases_u32(brute_force::<u32>);
+        test_cases_f64(brute_force::<f64>);
+    }
+
+    #[test]
+    fn test_kadane() {
+        test_cases_i32(kadane::<i32>);
+        test_cases_u32(kadane::<u32>);
+        test_cases_f64(kadane::<f64>);
     }
 }
